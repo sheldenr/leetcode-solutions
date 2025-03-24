488f58ee677f45e3db6cf06205a76f8a4cb29bab
SheldenR <bbshelden@gmail.com>
2025-03-24 15:28:01 -0400

    Added solution

--- easy/643-maximum_average_subarray_I.py
+++ easy/643-maximum_average_subarray_I.py
@@ -11,9 +11,12 @@ class Solution:
         # return max_avg
 
         # Second planned solution (idk i give up, lookup solution)
+        # I know that the idea is calculate the first window (0 thru k) and then add on the k + 1 element and subtract the element before the window
+        # do this and find the sum for each time so that it is a one pass and then return the greatest sum found
         max_sum = sum(nums[0:k])
+        current_sum = max_sum
 
-        for i in range(len(nums) - k):
+        for i in range(1, ):
             if max_sum + nums[k + i] - nums[i] > max_sum:
                 max_sum = max_sum + nums[k + i] - nums[i]
 
