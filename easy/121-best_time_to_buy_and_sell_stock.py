d875b2b509eb2208209addd1af1f02bb21fce813
SheldenR <bbshelden@gmail.com>
2025-04-17 15:06:03 -0400

    Added solution

--- easy/121-best_time_to_buy_and_sell_stock.py
+++ easy/121-best_time_to_buy_and_sell_stock.py
@@ -1,25 +1,15 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # # This was the initial solution I came up with to find min stock price
-	# minPrice = float("inf")
-        # maxProfit = 0
+        minPrice = float("inf")
+        maxProfit = 0
+        profit = 0
 
-        # for price in prices:
-            # minPrice = min(minPrice, price)
-            # maxProfit = max(price - minPrice, maxProfit)
-
-        # return maxProfit
-	
-	# Solution after learning more about leetcode and greedy approach
-	minPrice, maxProfit, curr_price = prices[0], float("-inf"), 0
-
-	for price in prices:
-    		if price < minPrice:
-		        minPrice = price
+        for price in prices:
+            if price < minPrice:
+                minPrice = price
             
-		if price - minPrice > maxProfit:
-                	maxProfit = price - minPrice
-
+            if (price - minPrice) > maxProfit:
+                maxProfit = (price - minPrice)
+            
         return maxProfit
 
-	# got it first try no way !!!
