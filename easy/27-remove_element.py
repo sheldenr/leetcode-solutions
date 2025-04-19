9c90ec2b46e93c5cfc30c53ae7e91cbdcb9989c1
SheldenR <bbshelden@gmail.com>
2025-04-19 12:37:01 -0400

    Added solution

---
+++ easy/27-remove_element.py
@@ -0,0 +1,7 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        # This is a non optimal O(n^2) approach from as far as I can tell
+        while val in nums:
+            nums.remove(val)
+
+        return len(nums)
