2a252402b3c306f1cfdf0755f8e9c8cac825b26f
SheldenR <bbshelden@gmail.com>
2025-05-04 23:29:23 -0400

    Added solution

--- hard/295-find_median_from_data_stream.py
+++ hard/295-find_median_from_data_stream.py
@@ -6,9 +6,13 @@ class MedianFinder:
 
     def addNum(self, num: int) -> None:
         heapq.heappush(self.max_heap, -num)
-        heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
 
-        if len(self.min_heap) > len(self.max_heap):
+        if self.min_heap and -self.max_heap[0] > self.min_heap[0]:
+            heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
+
+        if len(self.max_heap) > len(self.min_heap) + 1:
+            heapq.heappush(self.min_heap, -heapq.heappop(self.max_heap))
+        elif len(self.min_heap) > len(self.max_heap):
             heapq.heappush(self.max_heap, -heapq.heappop(self.min_heap))
 
     def findMedian(self) -> float:
