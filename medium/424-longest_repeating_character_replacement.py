287936ea4411e586adc983118ed3fb188fee4c09
SheldenR <bbshelden@gmail.com>
2025-05-14 19:26:07 -0400

    Updated solution

--- medium/424-longest_repeating_character_replacement.py
+++ medium/424-longest_repeating_character_replacement.py
@@ -1,16 +1,24 @@
 class Solution:
-    def characterReplacement(self, s: str, k: int) -> int:
-        character_map = {}
-        max_length = 0
-
+    def characterReplacement(self, s: str, k: int) -> int:  
+        character_map = { s[0] : 1 }
+        max_size = 1
         l = 0
-        for r in range(len(s)):
-            character_map[s[r]] = 1 + character_map.get(s[r], 0)
-        
-            while (r - l + 1) - max(character_map.values()) > k:
+
+        for r in range(1, len(s)):
+            # r always increases, if window size (r-l) - most_freq_char < k, keep l same, else increase l
+            if character_map.get(s[r]) == None:
+                character_map[s[r]] = 1
+            else:
+                character_map[s[r]] += 1
+            
+            largest_occurence = max(character_map.values())
+            
+
+            if (r - l) - largest_occurence >= k:
                 character_map[s[l]] -= 1
                 l += 1
+
+            max_size = max(max_size, (r - l + 1))
+
+        return max_size
             
-            max_length = max(max_length, r - l + 1)
-            
-        return max_length
