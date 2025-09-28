cb9cad606f4bc8dfd1335db341f1180fc7cff7cf
SheldenR <bbshelden@gmail.com>
2025-09-28 02:13:15 -0400

    Improved solution

--- easy/876-middle_of_linked_list.py
+++ easy/876-middle_of_linked_list.py
@@ -5,31 +5,14 @@
 #         self.next = next
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        length, index = 0, 0
-        curr_node = head
-
-        if not head.next:
-            return head
-
-        # Determine length of linked lsit
-        while curr_node:
-            length += 1
-            curr_node = curr_node.next
-
-        curr_node = head
-        curr_index = 0
-        
-        if length % 2 == 0:
-            middle_index = ((length + 1) // 2)
+        if head:
+            fast = head
+            slow = head
         else:
-            middle_index = ((length + 1) // 2) - 1
+            return None
 
-        while curr_node:
-            curr_index += 1
-            curr_node = curr_node.next
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
 
-            if curr_index == middle_index:
-                return curr_node
-
-            
-
+        return slow
