5b9a86ad7f254fca647f68f67e50732449ca1986
SheldenR <bbshelden@gmail.com>
2025-11-24 21:18:11 -0500

    Added solutions

--- medium/78-subsets.py
+++ medium/78-subsets.py
@@ -1,8 +1,16 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        res = [[]]
+        res = []
 
-        for num in nums:
-            res += [curr + [num] for curr in res]
-        
+        def dfs(index, path):
+            if index == (len(nums)):
+                res.append(path.copy())
+                return
+
+            dfs(index + 1, path + [nums[index]])
+            dfs(index + 1, path)
+
+        dfs(0, [])
         return res
+        
+
