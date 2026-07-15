5abf50a18544a2e554de266f9141f39c3b390c45
SheldenR <bbshelden@gmail.com>
2026-07-15 15:00:50 -0400

    added solution

---
+++ easy/3658-gcd_of_odd_and_even_sums.py
@@ -0,0 +1,15 @@
+class Solution:
+    def gcdOfOddEvenSums(self, n: int) -> int:
+        # sumOdd = n * n
+        # sumEven = n * (n + 1)
+
+        # def gcd(a, b):
+        #     while b != 0:
+        #         a, b = b, a % b
+        #     return a
+
+        # return gcd(sumOdd, sumEven)
+
+        # what a problem man, the answer is literally n LMAO
+
+        return n
